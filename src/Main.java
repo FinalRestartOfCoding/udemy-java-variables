2183ad50e35dc6b057cedb6a96dc0463fc11473a
FinalRestartOfCoding <finalrestartofcoding@gmail.com>
2022-10-28 00:20:08 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Initializing a int variable.
+        int myFirstNumber = 5;
+
+        // The assignment operator "=" is used to assign the value 5 to variable myFirstNumber.
+
+        // Initialization of a variable is OPTIONAL.
+        int a;
+
+        // Initializing the variable a of value 10.
+        a = 10;
+
+        // Prints the string literal myFirstNumber.
+        System.out.println("myFirstNumber");
+
+        // Prints the value stored in variable myFirstNumber.
+        System.out.println(myFirstNumber);
+
+        // Updating the value of variable myFirstNumber.
+        myFirstNumber = 10;
+
+        // Prints the value stored in variable myFirstNumber.
+        System.out.println(myFirstNumber);
+
+        // Updating the value of variable myFirstNumber via an expression (10 + 5).
+        myFirstNumber = 10 + 5;
+
+        // The above expression uses "+" (addition) operator to add two values.
+
+        // Prints the value stored in variable myFirstNumber.
+        System.out.println(myFirstNumber);
+
+        // Updating the value of variable myFirstNumber via an expression (10 + 5) + (2 * 10).
+        myFirstNumber = (10 + 5) + (2 * 10);
+
+        // The above expression contains multiple operators -> + (addition) and * (multiplication).
+
+        // Prints the value stored in variable myFirstNumber.
+        System.out.println(myFirstNumber);
+    }
+}
